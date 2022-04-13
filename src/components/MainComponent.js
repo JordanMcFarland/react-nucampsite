596a22d91b9d835f382323abdc4f9d0c3a63949f
Jordan McFarland <jdmcfarland4591@gmail.com>
2022-04-13 09:41:19 -0500

    Formatting

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,32 +8,32 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          campsites: CAMPSITES,
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+    };
+  }
+
+  render() {
+    const HomePage = () => {
+      return (
+        <Home />
+      );
     }
 
-    render() {
-        const HomePage = () => {
-            return (
-                <Home />
-            );
-        }
-
-        return (
-            <div>
-                <Header />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                    <Redirect to='/home' />
-                </Switch>
-                <Footer />
-            </div>
-        );
-    }
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+          <Redirect to='/home' />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Main;
